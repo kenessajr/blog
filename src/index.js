496f79753939d42e365744612b2444ba4c142e16
remymuhire <jeanremy1806@gmail.com>
2018-05-29 14:02:24 +0200

    removed two components

--- src/index.js
+++ src/index.js
@@ -6,26 +6,16 @@ import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom'
 
 import './index.css';
-import App from './components/App';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends React.Component {
-  render() { return <div>Hello!</div> }
-}
-
-class Goodbye extends React.Component {
-  render() { return <div>Goodbye!</div> }
-}
-
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/hello" component={Hello} />
-        <Route path="/goodbye" component={Goodbye} />
+        <PostsIndex />
       </div>
     </BrowserRouter>
   </Provider>,
