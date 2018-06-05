dc3859cbe5dd41ff31ce7543d8509bf0bea1e467
remymuhire <jeanremy1806@gmail.com>
2018-06-05 16:47:42 +0200

    post show fixed bugs

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPost } from '../actions';
+
 
 class PostsShow extends Component {
   componentDidMount() {
-    const { id } = this.props.match.parms.id;
+    const { id } = this.props.match.params;
     this.props.fetchPost(id);
+    console.log(id);
   }
 
   render() {
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading</div>;
+    }
+
     return (
       <div>
-        Posts Show!
+        <Link to="/">Back to Index</Link>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
 }
 
 function mapStateToProps({ posts }, ownProps) {
-  return { post: posts[ownProps.match.parms.id] };
+  return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fecthPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
